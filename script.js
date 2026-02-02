2556ff3c7d8c82ccd3f3980a58b72025dd76003a
parassmarok-alt <parassmarok@gmail.com>
2026-02-01 21:57:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "BALLEEEEEE!";
 
     catImg.src = "cat_dance.gif";
 
